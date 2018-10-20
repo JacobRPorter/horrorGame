e3d6b3aa683d79f8bc48493302dfcd21d52c2ee3
JacobRyanPorter <jacob.porter@promontech.com>
2018-10-20 16:43:20 -0600

    Updated so events are easier to control.

--- src/app/opening/opening.component.ts
+++ src/app/opening/opening.component.ts
@@ -7,24 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpeningComponent implements OnInit {
 
-  public gamestart: Boolean = false;
-  public continue1: Boolean = false;
+  public eventCount: number;
 
   constructor() { }
 
   ngOnInit() {
-    this.gamestart = true;
+    this.eventCount = 0;
+    this.gameContinue();
   }
 
+  public typeWriter(text, speed: number = 50, i: number = 0) {
+    if (i === 0) {
+      document.getElementById('blinker').insertAdjacentHTML('beforebegin', '<br>');
+    }
+    if (i < text.length) {
+      setTimeout(() => {
+        document.getElementById('blinker').insertAdjacentHTML('beforebegin', text.charAt(i));
+        i++;
+        this.typeWriter(text, speed, i);
+      }, speed);
+    }
+  }
 
-  public gameContinue(event) {
-    switch (event) {
+  public gameContinue() {
+    this.eventCount++;
+    switch (this.eventCount) {
       case 1: {
-        this.continue1 = true;
+        this.typeWriter(`When I first moved into the apartment`);
+        break;
+      }
+      case 2: {
+        this.typeWriter(`Well, let's go back a little bit`);
         break;
       }
     }
-
   }
 
 }
