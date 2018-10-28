5accd1f55c02c334fb25ee7ae95a6a4f0a028898
JacobRyanPorter <jacob.porter@promontech.com>
2018-10-28 09:46:14 -0600

    Updated the app component. Now we have a button to look at the window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'horror adventure';
+  public inside = true;
+  public eventCount: number;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.eventCount = 0;
+  }
+
+  public lookOutside() {
+    this.inside = false;
+  }
+
+  public lookAtComputer() {
+    this.inside = true;
+    this.gameContinue(true);
+  }
+
+  public typeWriter(text, speed: number = 50, i: number = 0) {
+    if (i === 0) {
+      document.getElementById('blinker').insertAdjacentHTML('beforebegin', '<br>');
+    }
+    if (i < text.length) {
+      setTimeout(() => {
+        document.getElementById('blinker').insertAdjacentHTML('beforebegin', text.charAt(i));
+        i++;
+        this.typeWriter(text, speed, i);
+      }, speed);
+    }
+  }
+
+  public gameContinue(resumedFromPrevious: boolean) {
+    if (!resumedFromPrevious) {
+      this.eventCount++;
+    }
+    switch (this.eventCount) {
+      case 1: {
+        this.typeWriter(`When I first moved into the apartment`);
+        break;
+      }
+      case 2: {
+        this.typeWriter(`Well, let's go back a little bit`);
+        break;
+      }
+    }
+  }
 }
--- src/app/opening/opening.component.ts
+++ src/app/opening/opening.component.ts
@@ -7,40 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpeningComponent implements OnInit {
 
-  public eventCount: number;
-
   constructor() { }
 
   ngOnInit() {
-    this.eventCount = 0;
-    this.gameContinue();
-  }
-
-  public typeWriter(text, speed: number = 50, i: number = 0) {
-    if (i === 0) {
-      document.getElementById('blinker').insertAdjacentHTML('beforebegin', '<br>');
-    }
-    if (i < text.length) {
-      setTimeout(() => {
-        document.getElementById('blinker').insertAdjacentHTML('beforebegin', text.charAt(i));
-        i++;
-        this.typeWriter(text, speed, i);
-      }, speed);
-    }
-  }
-
-  public gameContinue() {
-    this.eventCount++;
-    switch (this.eventCount) {
-      case 1: {
-        this.typeWriter(`When I first moved into the apartment`);
-        break;
-      }
-      case 2: {
-        this.typeWriter(`Well, let's go back a little bit`);
-        break;
-      }
-    }
   }
 
 }
